4b6d96968d3e91559e2e1434bb1cc9692c9349a7
hyeyeoung <rlagpdud0524@gmail.com>
2025-01-16 22:14:31 +0900

    mvvm pattern & DBlink

--- batteryQI/ViewModels/Bases/ViewModelBases.cs
+++ batteryQI/ViewModels/Bases/ViewModelBases.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using batteryQI.Models;
 
 namespace batteryQI.ViewModels.Bases
 {
-    internal class ViewModelBases : ObservableObject
+    internal partial class ViewModelBases : ObservableObject
     {
+        // 클릭 이벤트 등록
+        [RelayCommand]
+        private void LinkDB()
+        {
+            DBlink x = new(); 
+            x.Connect(); // 링크
+        }
     }
 }
