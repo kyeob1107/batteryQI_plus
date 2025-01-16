85b8c2838fa250f41c2fdcf757366841722704eb
hyeyeoung <rlagpdud0524@gmail.com>
2025-01-16 23:28:38 +0900

    DataBase

---
+++ batteryQI/Models/DBlink.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+
+namespace batteryQI.Models
+{
+    internal class DBlink : ObservableObject
+    {
+
+        private string _server = ""; // _server : ip 주소
+        private string _port = ""; // _port : 포트번호
+        private string _dbName = ""; // _dbName : 연결 스키마
+        private string _dbId = ""; // _dbId : 접속아이디
+        private string _dbPw = ""; // _dbpw : 접속패스워드
+
+        private void setDBLink()
+        {
+            string relativePath = @".\Models\DB.txt";
+            string fullPath = Path.Combine(Environment.CurrentDirectory, relativePath);
+            StreamReader sr = new StreamReader(fullPath);
+            string[] Data = sr.ReadToEnd().Split("\n");
+            _server = Data[0];
+            _port = Data[1];
+            _dbName = Data[2];
+            _dbId = Data[3];
+            _dbPw = Data[4];
+        }
+        public void Connect()
+        {
+            this.setDBLink();
+            string myConnection = "Server="+_server + ";Port=" + _port + ";Database=" + _dbName + ";User Id = " + _dbId + ";Password = " + _dbPw;
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(myConnection))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM manager"; // 테스트 쿼리
+
+                    // ExecuteReader를 이용하여
+                    // 연결 모드로 데이터 가져오기
+                    MySqlCommand cmd = new MySqlCommand(sql, connection);
+                    MySqlDataReader table = cmd.ExecuteReader();
+                    string x = "";
+                    while (table.Read())
+                    {
+                        x += $"{table["managerId"]}\r\n";
+                    }
+                    table.Close();
+                    MessageBox.Show(x);
+                }
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.ToString());
+            }
+        }
+    }
+}
