bc88dc25a13bbc7a620119a83d7c843ef5be8249
kyeob1107 <koak1107@gmail.com>
2025-01-17 18:21:31 +0900

    [updata] battery.cs 파일 기본 선언추가

--- batteryQI/Models/Battery.cs
+++ batteryQI/Models/Battery.cs
@@ -7,8 +7,32 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace batteryQI.Models
 {
+    public struct BatteryData
+    {
+        int batteryId;
+        DateTime shootDate;
+        string usage;
+        string batteryType;
+        int manufacId;
+        string batteryShape;
+        string shootPlace;
+        string imagePath;
+        int managerNum;
+        bool defectStat;
+        string defectName;
+    }
+
     internal class Battery : ObservableObject
     {
+        BatteryData BD {  get; set; } = new BatteryData();
+        void batteryInput( BatteryData bd)
+        {
+            //DB에 insert관련?
+        }
 
+        void imgProcessing()
+        {
+            //AI를 통해서 처리하는 부분
+        }
     }
 }
