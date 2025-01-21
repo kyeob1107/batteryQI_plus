b3a8bd97d7d6e7a7aabdba32e6b4f0b1b00b6cea
hyeyeoung <rlagpdud0524@gmail.com>
2025-01-21 09:48:49 +0900

    파일 정리, manager field 추가

--- batteryQI/Models/Battery.cs
+++ batteryQI/Models/Battery.cs
@@ -19,23 +19,18 @@ namespace batteryQI.Models
     internal class Battery : ObservableObject
     {
         // 배터리 가용 변수 선언(변동사항 있음. Id나 date 데이터 등은 DB에 기입할 때 필요하지만 관리자가 건들이는 부분은 없음
-        //private int _batteryId; // 자동 기입 -> DB에서 기입될 뿐 코드에선 사용 x
-        //private DateTime _shootDate; // 자동 기입, 굳이 private으로 선언할 필요 x
         private string _usage = ""; // 사용처, 직접 기입
         private string _batteryType = ""; // 직접 기입
         private string _manufacName = "";
-        //private string _manufacId; // 직접 기입(제조사 id를 가져옴)
         private string _batteryShape = ""; // 직접 기입
         private string _shootPlace = "line1"; // 자동 기입(일단은)
         private string? _imagePath = ""; // 파일 로드 경로 저장 -> 이미지 로드
-        //private int _managerNum; // 자동 기입(현재 로그인 되어 있는 관리자로)
         private string _defectStat; // 직접 기입(불량, 정상 선택으로)
         private string _defectName; // 직접 기입
         // -------------
         // 배터리 객체도 싱글톤으로 수행
         // 사유: 여러 개의 배터리 객체를 관리하는 것이 아닌 순차적으로 배터리 객체를 활용하기 때문
-        // (현재 검사 결과를 수행하지 않으면 다음 검사 불가, 이전 검사 결과는 휘발됨), 메모리 활용성을 위해서 재사용 하는 것으로 로직 변경
-
+        
         private string modelPath = @".\weight\deeplab_model5.onnx"; // 이미지 처리 모델 로드
 
         static Battery staticBattery;
