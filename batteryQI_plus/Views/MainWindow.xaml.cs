01f03dedbfdff08bd0112c5634ce6141c004f1c8
hajun05 <hajun0325175@gmail.com>
2025-02-27 12:18:52 +0900

    MainWindow View의 창 닫기 기능을 ViewModel에 연결하는 부분의 오류 수정(ManagerViewModel vm -> MainWindowViewModel vm)

--- batteryQI_plus/Views/MainWindow.xaml.cs
+++ batteryQI_plus/Views/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace batteryQI.Views
             //var viewModel = new MainWindowViewModel();
             //viewModel.CloseAction = () => this.Close();
             //this.DataContext = viewModel;
-            if (DataContext is ManagerViewModel vm)
+            if (DataContext is MainWindowViewModel vm)
             {
                 vm.CloseAction = () => this.Close(); // 창 닫기 기능을 ViewModel에 연결
             }
